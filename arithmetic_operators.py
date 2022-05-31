111b2c9681b164cc324e0d0b98f22f40e3da2b69
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-05-31 09:37:18 -0700

    Update arithmetic_operators.py

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,4 +1,11 @@
 '''
+Task:
+  The provided code stub reads two integers from STDIN, "a" and "b". Add code to print three lines where:
+
+  1) The first line contains the sum of the two numbers.
+  2) The second line contains the difference of the two numbers (first - second).
+  3) The third line contains the product of the two numbers.
+  
 Constraints:
   a: 1<= a <= 10**10
   b: 1<= b <= 10**10
