5127be526f4e454724178a555fceb7d7cdb8f1a5
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-05-31 12:23:40 -0700

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,3 +1,15 @@
+'''
+Task:
+    Given an integer, "n", perform the following conditional actions:
+
+    -If "n" is odd, print "Weird"
+    -If "n" is even and in the inclusive range of 2 to 5, print "Not Weird"
+    -If "n" is even and in the inclusive range of 6 to 20, print "Weird"
+    -If "n" is even and greater than 20, print Not Weird
+    
+Constraints:
+    1 <= n <= 100
+'''
 #!/bin/python
 
 import math
