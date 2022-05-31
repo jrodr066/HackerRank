6b45e6d7dc76a4499c01b0e55e5e0bd1f92fedc9
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-05-31 13:20:10 -0700

    Update print_function.py

--- print_function.py
+++ print_function.py
@@ -1,3 +1,13 @@
+'''
+Task:
+    The included code stub will read an integer, "n", from STDIN.
+    Without using any string methods, try to print the following:
+        123 ... "n"
+    Note that "" represents the consecutive values in between.
+    
+Constraints:
+    1 <= n <= 150
+'''
 n = int(5)
 for i in range(1,n+1):
     print(i, end ="")
