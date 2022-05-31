2fc333eddfaf75a6be15afe01749d4ae25e5fb4d
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-05-31 09:34:27 -0700

    Adding arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,13 @@
+'''
+Constraints:
+  a: 1<= a <= 10**10
+  b: 1<= b <= 10**10
+'''
+a = int(5)
+b = int(2)
+if a>=1 or b>=1:
+    print(a + b)
+    print(a - b)
+    print(a * b)
+else:
+    print("Not in range.") 
