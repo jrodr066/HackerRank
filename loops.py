65a6063d1331d616d7184afa2747db66a7719548
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-05-31 12:19:49 -0700

    Adding loops.py

---
+++ loops.py
@@ -0,0 +1,14 @@
+'''
+Task:
+The provided code stub reads and integer,"n", from STDIN. For all non-negative integers i<n, print "i**2" .
+
+Constraints:
+
+1<= n <= 20
+'''
+
+
+n = int(3)
+for i in range(0,21):
+    if i<n:
+        print(i**2) 
