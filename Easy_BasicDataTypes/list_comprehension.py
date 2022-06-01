c9304e75b9435ed4dfb590c172ae4c23b6a5ec43
jrodr066 <63874334+jrodr066@users.noreply.github.com>
2022-06-01 08:42:55 -0700

    Update list_comprehension.py

--- Easy_BasicDataTypes/list_comprehension.py
+++ Easy_BasicDataTypes/list_comprehension.py
@@ -1,3 +1,7 @@
+'''
+Task:
+    Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+'''
 x = int(1)
 y = int(1)
 z = int(1)
